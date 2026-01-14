52321dfcde4b66ec42313b3c98fe3e286c640614
Kilian Schopfer <pb20dsi@eduvaud.ch>
2026-01-14 09:35:51 +0100

    feat(app Vue) : création de l'application Vue théorie + exercices
    [40][done]

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Smells too much and not good'
+        }
+    }
+});
